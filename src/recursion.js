16b225a6ed3f1efc722bfa2bd8dcd1e6009283ea
nelliott82 <nikkoelliott@gmail.com>
2022-03-28 17:15:27 -0400

    Passed divide tests

--- src/recursion.js
+++ src/recursion.js
@@ -199,6 +199,36 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (x === 0 && y === 0) {
+    return NaN;
+  }
+  if (x === 0) {
+    return x;
+  }
+  if (x < 0 && y < 0) {
+    if (x > y) {
+      return 0;
+    }
+    return 1 + (divide(x - y, y));
+  }
+  if (x > 0 && y > 0) {
+    if (x < y) {
+      return 0;
+    }
+    return 1 + (divide(x - y, y));
+  }
+  if (x > 0 && y < 0) {
+    if (x < -y) {
+      return 0;
+    }
+    return -1 + (divide(x + y, y));
+  }
+  if (x < 0 && y > 0) {
+    if (-x < y) {
+      return 0;
+    }
+    return -1 + (divide(x + y, y));
+  }
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
