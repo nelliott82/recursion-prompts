98b84ceb9ffc3c22ad0cafdc03c6530009bcd7b9
nelliott82 <nikkoelliott@gmail.com>
2022-03-30 12:06:05 -0400

    Passed augmentElements tests

--- src/recursion.js
+++ src/recursion.js
@@ -521,6 +521,13 @@ var compress = function(list) {
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+  if (array.length === 0) {
+    return [];
+  }
+  var returnArray = [];
+  array[0].push(aug);
+  returnArray.push(array[0]);
+  return returnArray.concat(augmentElements(array.slice(1), aug));
 };
 
 // 34. Reduce a series of zeroes to a single 0.
